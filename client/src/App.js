cddeb268877cb1fe18a8844945fc57b10ae06252
tsdmrfth <line.939018>
2019-01-07 05:43:35 +0300

    Added socket.io to projects.

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import List from './pages/List';
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://localhost:3400"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => alert(data));
+  }
+
   render() {
     const App = () => (
       <div>
