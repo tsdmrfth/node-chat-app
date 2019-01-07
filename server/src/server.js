66626b02dc308fb27a4bfac14012759db881c860
tsdmrfth <line.939018>
2019-01-07 05:03:55 +0300

    Update project folders.

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/build'));
 });
-
+console.log(path.join(__dirname, '../../client/build'))
 app.get('/api/getList', (req, res) => {
     var list = ["item2", "baba", "item3"];
     res.json(list);
